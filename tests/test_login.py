d0bfb5bf5f672080b764c1431d0f0d6e206423b9
Carlosara142 <carlosara142@gmail.com>
2026-06-22 18:19:38 -0300

    login pages

---
+++ tests/test_login.py
@@ -0,0 +1,10 @@
+from pages.login_page import LoginPage
+
+from data.users import USER
+import pytest
+
+@pytest.mark.parametrize("username, password", USER)
+def test_login(driver, username, password  ):
+    login_page = LoginPage(driver)
+    login_page.open()
+    login_page.login(username, password)
--- utils/helpers.py
+++ utils/helpers.py
@@ -10,17 +10,3 @@ def get_driver():
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service)
     return driver
-
-
-def login(driver, username, password):
-
-    wait = WebDriverWait(driver , 10)
-
-    driver.get("https://www.saucedemo.com/")
-
-    wait.until(
-        EC.presence_of_element_located((By.ID, "user-name"))
-    ).send_keys(username)
-
-    driver.find_element(By.ID , "password").send_keys(password)
-    driver.find_element(By.ID , "login-button").click()
