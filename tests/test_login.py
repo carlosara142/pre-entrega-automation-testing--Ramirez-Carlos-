0fe27baf8aa95886e23a84cf4f85e84c73c503dc
Carlosara142 <carlosara142@gmail.com>
2026-06-23 15:24:21 -0300

    agragado el csv y json para test de login

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,8 +1,13 @@
 from pages.login_page import LoginPage
-from data.users import USER
+#from data.users import USER
+from utils.helpers import load_user_csv, load_user_jason
 import pytest
 
-@pytest.mark.parametrize("username, password", USER)
+load_csv = load_user_csv("data/users.csv")
+load_json = load_user_jason("data/users.json")
+
+#al final de carga del csv o json, se puede usar la lista de usuarios para parametrizar el test de login
+@pytest.mark.parametrize("username, password", load_json)
 def test_login(driver, username, password  ):
     login_page = LoginPage(driver)
     login_page.open()
--- utils/helpers.py
+++ utils/helpers.py
@@ -4,9 +4,29 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+import csv
+import json
 
 def get_driver():
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service)
     return driver
+
+def load_user_csv(user_csv_path):
+    users = []
+    with open(user_csv_path, newline='') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            if row['username'] and row['password']:
+                users.append((row['username'], row['password']))
+    return users
+
+def load_user_jason(user_csv_path):
+    users = []
+    with open(user_csv_path, newline='') as file:
+        data = json.load(file)  
+
+        for user in data:
+            if 'username' in user and 'password' in user:
+                users.append((user['username'], user['password']))  
+    return users
